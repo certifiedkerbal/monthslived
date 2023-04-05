3f421724f4d5d4aaeb8d35b80c165e590be6ce9a
certifiedkerbal <87107412+certifiedkerbal@users.noreply.github.com>
2023-04-04 17:45:42 -0700

    Add files via upload

---
+++ monthslived.py
@@ -0,0 +1,6 @@
+years_str = input('Amount of years you have lived ')
+months_str = input('Amount of months since your last birthday ')
+years = int(years_str)
+months = int(months_str)
+totalMonthsLived = months + years * 12
+print (totalMonthsLived)
